efff89613c257e46a882eeafc14fd954b791becf
Sophy Pistun <sophypistun@gmail.com>
2022-06-24 15:09:29 +0300

    Made changes to avoid errors in the presence of characters that are not in the Ukrainian alphabet.

--- task_file.py
+++ task_file.py
@@ -1,9 +1,9 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Transliteration')
-parser.add_argument("-s", "--sentence", type=str, help=("Input your phrase for transliteration"), required=True)
+parser = argparse.ArgumentParser(description='Transliteration from Ukrainian letters to Latin letters')
 parser.add_argument("input_file", type=str, help=("Input file to read"))
 parser.add_argument("output_file", type=str, help=("Input file to write"))
+parser.add_argument("-s", "--sentence", type=str, help=("Input your phrase for transliteration in quotation marks"), required=True)
 
 args = parser.parse_args()
 
@@ -20,8 +20,17 @@ def create_dict_for_translit(input_file):
 def transliterate(dict_for_translit, sentence):
     sentence = list(sentence)
     translit_result = ''
+    wrong_input_indicator = False
     for element in sentence:
-        translit_result = translit_result + dict_for_translit[element]
+        if element in dict_for_translit.keys():            
+            translit_result = translit_result + dict_for_translit[element]
+        elif element == ' ' or element == ',' or element == '.' or element == '!' or element == '?' or element == '-':
+            translit_result = translit_result + element
+        else:
+            translit_result = translit_result + element
+            wrong_input_indicator = True
+    if wrong_input_indicator:        
+        print('\nThe whole phrase or some of its parts were not entered in the letters of the Ukrainian alphabet. These parts remained unchanged.')
     return translit_result
 
 
@@ -33,4 +42,4 @@ dict_for_translit = create_dict_for_translit(args.input_file)
 translit_result = transliterate(dict_for_translit, args.sentence)
 save_in_file(args.output_file, translit_result)
 
-print(translit_result)
+print('Transliterated phrase: ', translit_result, '\n')
